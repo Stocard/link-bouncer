ab1ca8d7011b6e35febb7775c74c8a3a4646cdfa
Mark Himmelsbach <himmelsbach@stocard.de>
2014-11-12 16:36:54 +0100

    send non-mobile os to homepage instead app store

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var locale = require("locale");
 var url = require('url');
-var Mixpanel = require('mixpanel');
+var Mixpanel = require('Mixpanel');
 
 module.exports = createMiddleware;
 
@@ -27,20 +27,14 @@ function createMiddleware(trackFn, redirectLinks) {
     if (req.headers["user-agent"].indexOf("Android") > -1) {
         res.header("Location", redirectLinks.Android);
         analyticsDict["$os"] = "Android";
-    } else if (req.headers["user-agent"].indexOf("Windows") > -1) {
-        res.header("Location", redirectLinks.Windows);
-        analyticsDict["$os"] = "Windows Phone";
     } else if (req.headers["user-agent"].indexOf("iPhone") > -1) {
         res.header("Location", redirectLinks.iPhone);
         analyticsDict["$os"] = "iPhone OS";
-    } else if (req.headers["user-agent"].indexOf("Macintosh") > -1) {
-        res.header("Location", redirectLinks.iPhone);
-        analyticsDict["$os"] = "Mac OS";
-    } else if (req.headers["user-agent"].indexOf("Linux") > -1) {
-        res.header("Location", redirectLinks.Android);
-        analyticsDict["$os"] = "Linux";
+    } else if (req.headers["user-agent"].indexOf("Windows Phone") > -1) {
+        res.header("Location", redirectLinks.Windows);
+        analyticsDict["$os"] = "Windows Phone";
     } else {
-        res.header("Location", redirectLinks.iPhone);
+        res.header("Location", "http://www.stocardapp.com");
         analyticsDict["$os"] = "other";
     }
     
