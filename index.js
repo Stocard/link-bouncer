aeadb808a07d0bdb024d39d1f2e505e5eabdfbef
Florian Barth <barth@stocard.de>
2014-12-05 11:26:22 +0100

    Migrated bouncer from 302 to meta-refresh

--- index.js
+++ index.js
@@ -1,46 +1,64 @@
 
-var locale = require("locale");
-var url = require('url');
-var Mixpanel = require('Mixpanel');
+var locale = require("locale")
+var url = require('url')
+var Mixpanel = require('Mixpanel')
+var mustache = require('mustache')
+var fs = require('fs')
 
-module.exports = createMiddleware;
+module.exports = createMiddleware
 
 function createMiddleware(trackFn, redirectLinks) {
-  return trackShareClick;
+
+  return trackShareClick
 
   function trackShareClick(req, res, next) {
-    var analyticsDict = {};
+    var analyticsDict = {}
     // compose analytics properties
-    var sender_id = req.query.s;
+
+    // track sender_id
+    var sender_id = req.query.s
     if (sender_id) {
-      analyticsDict["sender_id"] = sender_id;
+      analyticsDict["sender_id"] = sender_id
     }
+    // track languages
     if (req.headers["accept-language"]) {
-        var userLocale = new locale.Locales(req.headers["accept-language"]).best();
-        analyticsDict["language code"] = userLocale.language;
+        var userLocale = new locale.Locales(req.headers["accept-language"]).best()
+        analyticsDict["language code"] = userLocale.language
         if (userLocale.country) {
-            analyticsDict["mp_country_code"] = userLocale.country;
-        }
+            analyticsDict["mp_country_code"] = userLocale.country
+      }
     }
 
     // identify bounce url
+    var target_url = "http://www.stocardapp.com"
     if (req.headers["user-agent"].indexOf("Android") > -1) {
-        res.header("Location", redirectLinks.Android);
-        analyticsDict["$os"] = "Android";
+      target_url = redirectLinks.Android
+      analyticsDict["$os"] = "Android"
     } else if (req.headers["user-agent"].indexOf("iPhone") > -1) {
-        res.header("Location", redirectLinks.iPhone);
-        analyticsDict["$os"] = "iPhone OS";
+      target_url = redirectLinks.iPhone
+      analyticsDict["$os"] = "iPhone OS"
     } else if (req.headers["user-agent"].indexOf("Windows Phone") > -1) {
-        res.header("Location", redirectLinks.Windows);
-        analyticsDict["$os"] = "Windows Phone";
-    } else {
-        res.header("Location", "http://www.stocardapp.com");
-        analyticsDict["$os"] = "other";
+      target_url = redirectLinks.Windows
+      analyticsDict["$os"] = "Windows Phone"
     }
-    
+
+    // generate response template
+    var bouncerTemplate = fs.readFileSync("./bouncer-template.html", "utf8")
+    mustache.parse(bouncerTemplate)
+    var values = {
+      "target_url" : target_url,
+    }
+    var content = mustache.render(bouncerTemplate, values)
+
+    res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(content),
+      'Content-Type': 'text/html'
+    })
+
     // track and bounce
-    trackFn("share link visited", analyticsDict);
-    res.send(302);
-    if (next) next();
+    trackFn("share link visited", analyticsDict)
+    res.write(content)
+    res.send()
+    if (next) next()
   }
 }
--- test.js
+++ test.js
@@ -1,48 +1,59 @@
 
-var assert = require('assert');
+var assert = require('assert')
+var fs = require('fs')
+var mustache = require('mustache')
 
-var createMiddleware = require('./index');
+var createMiddleware = require('./index')
 
 var redirectLinks = {
   iPhone: 'http://iphone',
   Android: 'http://android',
   Windows: 'http://windows'
-};
+}
 
 var mixpanelOSProperty = {
   iPhone: 'iPhone OS',
   Android: 'Android',
   Windows: 'Windows Phone'
-};
+}
 
 describe('link-bouncer middleware', function () {
-  createTest('iPhone', 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X)');
-  createTest('Android', 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B)');
-  createTest('Windows', 'Windows');
+  createTest('iPhone', 'Mozilla/5.0 (iPhone CPU iPhone OS 5_0_1 like Mac OS X)')
+  createTest('Android', 'Mozilla/5.0 (Linux Android 4.0.4 Galaxy Nexus Build/IMM76B)')
+  createTest('Windows', 'Windows Phone')
 
   function createTest(testName, userAgent) {
     it('should track share with ' + userAgent + ' source', function (done) {
-      var tracked = false;
-      var middleware = createMiddleware(track, redirectLinks);
-      var senderID = 'abc';
-      var req = createFakeRequest(userAgent, {s: senderID});
-      var res = createFakeResponse(onSend);
-      middleware(req, res, function () {});
+      var tracked = false
+      var middleware = createMiddleware(track, redirectLinks)
+      var senderID = 'abc'
+      var req = createFakeRequest(userAgent, {s: senderID})
+      var res = createFakeResponse(onEnd)
+      middleware(req, res, function () {})
 
       function track(event, properties) {
-        assert.equal(event, 'share link visited');
-        assert.deepEqual(properties, {'sender_id':senderID, '$os':mixpanelOSProperty[testName]});
-        tracked = true;
+        assert.equal(event, 'share link visited')
+        assert.deepEqual(properties, {'sender_id':senderID, '$os':mixpanelOSProperty[testName]})
+        tracked = true
       }
-      function onSend(status) {
-        assert.equal(status, 302);
-        assert.ok(tracked);
-        assert.deepEqual(res.headers, [{key: 'Location', value: redirectLinks[testName]}]);
-        done();
+      function onEnd() {
+        assert.equal(res.status, 200)
+        assert.ok(tracked)
+        assert.equal(res.content, getContent(redirectLinks[testName]))
+        done()
       }
-    });
+
+      function getContent(target_url){
+        var bouncerTemplate = fs.readFileSync("./bouncer-template.html", "utf8")
+        mustache.parse(bouncerTemplate)
+        var values = {
+          "target_url" : target_url,
+        }
+        return mustache.render(bouncerTemplate, values)
+      }
+    })
   }
-});
+})
 
 function createFakeRequest(userAgent, query) {
   return {
@@ -50,15 +61,24 @@ function createFakeRequest(userAgent, query) {
     headers: {
       'user-agent': userAgent
     }
-  };
+  }
 }
 
 function createFakeResponse(onSend) {
-  return {
+  var that = {
     headers: [],
-    header: function (key, value) {
-      this.headers.push({key: key, value: value});
+    status : /*over*/9000,
+    content : "",
+    writeHead : function(status, headers){
+      Object.keys(headers).forEach(function(key){
+        that.headers.push({key: key, value: headers[key]})
+      })
+      that.status = status
+    },
+    write : function(content){
+      that.content = content
     },
     send: onSend
   }
+  return that
 }
